3844f81575b83f405a877e80d91b4843d0cc0199
prodonik <rruzibayevich@gmail.com>
2024-06-05 23:51:28 +0500

    created

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ruziba3vich/realtime
+
+go 1.22.3
+
+require golang.org/x/net v0.26.0
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"golang.org/x/net/websocket"
+)
+
+type (
+	Server struct {
+		conns map[*websocket.Conn]bool
+	}
+)
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func main() {
+
+}
